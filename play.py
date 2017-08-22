79362eeaa530661b5c645af977d5bc0f730cffab
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2017-08-22 20:24:10 +0200

    Improve hardcoded strategy

--- play.py
+++ play.py
@@ -4,17 +4,11 @@ import time
 env = gym.make('CartPole-v0')
 observation = env.reset()
 
-print(env.action_space)
-
-print(env.observation_space)
-print(env.observation_space.high)
-print(env.observation_space.low)
-
 for t in range(1000):
     print observation
 
     env.render()
-    observation, reward, done, info = env.step(t % 2)
+    observation, reward, done, info = env.step(observation[3] >= 0)
 
     if done:
         print "Episode finished after %d timesteps" % (t+1)
